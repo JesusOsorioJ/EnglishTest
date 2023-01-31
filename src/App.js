0611b6ad57a46db9595668aac57b6ea608ab862f
unknown <jesdaos@hotmail.com>
2023-01-30 19:39:24 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Home from './pages/Hom';
 import Subtitle from './pages/Subtitle'
 import InfoTest from './pages/InfoTest';
 import Tester from './pages/Tester';
